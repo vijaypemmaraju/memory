0deb233b78454915ef63822ee67a652f46352367
Vijay Pemmaraju <vijayvmp@gmail.com>
2016-12-14 16:54:04 -0800

    fined tuned AI and added comments

--- public/javascripts/Computer.js
+++ public/javascripts/Computer.js
@@ -1,18 +1,59 @@
 var computer = new Player('computer');
+
+/**
+ * Keeps track of all the cards the computer has seen, either by flipping cards itself or by observing the player's flips.
+ * @type {Array}
+ */
 computer.knownCards = [];
-computer.difficulty = 5;
-$('.computer-difficulty').change(function() {
-  computer.difficulty = parseInt($(this).val());
-})
 for (var i = 0; i < 14; i++) {
   computer.knownCards.push([])
 }
 computer.addKnownCard = function(card) {
-  if (computer.knownCards[card.getRawNumber()].indexOf(card) == -1) {
+  if (computer.knownCards[card.getRawNumber()].indexOf(card) === -1) {
     computer.knownCards[card.getRawNumber()].push(card);
   }
 }
+
+/**
+ * Essentially a multiplier for the match rate. 
+ * A difficulty of 10 means that the computer 
+ * will always choose a match it knows about, 
+ * whereas a difficulty of 5 means it will do 
+ * so with a 50% probability.
+ * @type {Number}
+ */
+computer.difficulty = 5;
+
+$('.computer-difficulty').change(function() {
+  computer.difficulty = parseInt($(this).val());
+})
+
+/**
+ * Find a match amongst known cards
+ */
+computer.findMatches = function() {
+  return computer.knownCards
+  .filter(function(a){return a.length >= 2})
+  .reduce(function(a,b) {return a.concat(b)}, []);
+}
+
+/**
+ * Choose the matched cards with a probability based on the difficulty setting
+ */
+computer.hasMatch = function(matches) {
+  return matches.length >= 2 && Math.random() < 0.1 * computer.difficulty;
+}
+
+/**
+ * This is where all of the AI logic happens. 
+ * The computer keeps track of all cards it has seen,
+ * and either chooses matches it knows about (with a 
+ * probability weighted by the difficulty setting)
+ * or chooses cards randomly.
+ */
 computer.makeMoves = function() {
+
+  // Clear out any known cards that have already been matched
   for (var i = 0; i < 14; i++) {
     for (var j = computer.knownCards[i].length - 1; j >= 0; j--) {
       if (computer.knownCards[i][j].matched) {
@@ -20,9 +61,6 @@ computer.makeMoves = function() {
       }
     }
   }
-  var matches = computer.knownCards
-  .filter(function(a){return a.length >= 2})
-  .reduce(function(a,b) {return a.concat(b)}, []);
 
   var firstMatchableCardIndex, secondMatchableCardIndex;
 
@@ -30,30 +68,49 @@ computer.makeMoves = function() {
     return !card.matched
   });
 
-  if (matches.length >= 2 && Math.random() < 0.1 * computer.difficulty) {
+  var matches = computer.findMatches();
+
+  if (computer.hasMatch(matches)) {
     firstMatchableCardIndex = unmatchedCards.indexOf(matches[0]);
     secondMatchableCardIndex = unmatchedCards.indexOf(matches[1]);
   }
 
-  if (unmatchedCards.length == 2) {
+  // Edge case for when the game is down to two cards
+  if (unmatchedCards.length === 2) {
     firstMatchableCardIndex = 0;
     secondMatchableCardIndex = 1;
   }
 
 
   setTimeout(function() {
-    var firstChosenCardIndex = firstMatchableCardIndex || Math.floor(Math.random() * unmatchedCards.length);
+    // Choose the matched card if present, otherwise choose randomly.
+    var firstChosenCardIndex = firstMatchableCardIndex !== undefined ? firstMatchableCardIndex : Math.floor(Math.random() * unmatchedCards.length);
     var firstCardLocation = cards.indexOf(unmatchedCards[firstChosenCardIndex]);
     var firstCard = cards[firstCardLocation];
 
     computer.addKnownCard(firstCard);
 
     $(firstCard.getElement()).click();
+
     setTimeout(function() {
       var secondChosenCardIndex;
-      do {
-        secondChosenCardIndex = secondMatchableCardIndex || Math.floor(Math.random() * unmatchedCards.length);
-      } while (secondChosenCardIndex == firstChosenCardIndex);
+
+      // Remove first card we chose so we don't choose it again
+      if (unmatchedCards.length > 2) {
+      unmatchedCards.splice(firstChosenCardIndex, 1);
+      }
+
+      // Recalculate matches after flipping the first card.
+      matches = computer.findMatches();
+      if (computer.hasMatch(matches)) {
+        if (firstCard === matches[0]) {
+          secondMatchableCardIndex = unmatchedCards.indexOf(matches[1]);
+        } else {
+          secondMatchableCardIndex = unmatchedCards.indexOf(matches[0]);
+        }
+      }
+      
+      secondChosenCardIndex = secondMatchableCardIndex !== undefined ? secondMatchableCardIndex :Math.floor(Math.random() * (unmatchedCards.length - 1));
       var secondCardLocation = cards.indexOf(unmatchedCards[secondChosenCardIndex]);
       var secondCard = cards[secondCardLocation];
 
--- public/javascripts/memory-game.js
+++ public/javascripts/memory-game.js
@@ -27,10 +27,20 @@ cardFiles.forEach(function(cardFile) {
 
 function renderSidebar() {
   $('.' + currentPlayer.name + '-match-count span').html(currentPlayer.matchCount);
-  if (gameState.yourTurn) {
-    $('.turn').text('It is your turn.');
+  if (isGameOver()) {
+    if (player.matchCount > computer.matchCount) {
+      $('.status').text('You Win!');
+    } else if (player.matchCount < computer.matchCount){
+      $('.status').text('The Computer Wins!');
+    } else {
+      $('.status').text('It\'s a tie!');
+    }
   } else {
-    $('.turn').text('It is the computer\'s turn.');
+    if (gameState.yourTurn) {
+      $('.status').text('It is your turn.');
+    } else {
+      $('.status').text('It is the computer\'s turn.');
+    }
   }
 }
 
@@ -49,54 +59,57 @@ function isCurrentPlayer(e) {
   return (currentPlayer == player && e.originalEvent && player.hasControl) || (currentPlayer == computer && e.originalEvent == null)
 }
 
-
+function isGameOver() {
+  return player.matchCount + computer.matchCount === cardFiles.length/2;
+}
 
 $(document).on('click', '.cards .card', function(e) {
   if (isCurrentPlayer(e)) {
     var selected = cards[$(this).index()];
-    if (selected.flipped && flippedCards.length == 1) {
+
+
+    if (selected.flipped) {
       return;
     }
-    selected.flipped = !selected.flipped;
-    if (selected.flipped) {
-      flippedCards.push(selected);
-      if (flippedCards.length >= 2) {
-        if (flippedCards[0].number === flippedCards[1].number) {
-          currentPlayer.hasControl = false;
-          setTimeout(function() {
-            currentPlayer.hasControl = true;
-            var $matchedCardPair = $('<div class="matched-card-pair"></div>');
-            currentPlayer.matchCount++;
-            flippedCards.forEach(function(flippedCard) {
-              flippedCard.matched = true;
-              flippedCard.getElement().replaceWith(Card.getBlankSpace());
-              $matchedCardPair.append(flippedCard.getHtml());
-            })
-            $('.' + currentPlayer.name + '-matched-cards').append($matchedCardPair);
-            if (currentPlayer === computer) {
-              computer.makeMoves();
-            }
-            flippedCards = [];
-            renderSidebar();
-          }, 50);
-        } else {
-          currentPlayer.hasControl = false;
-          setTimeout(function() {
-            currentPlayer.hasControl = true;
-            flippedCards.forEach(function(flippedCard) {
-              flippedCard.flipped = !flippedCard.flipped;
-              flippedCard.getElement().replaceWith(flippedCard.getHtml())
-              computer.addKnownCard(flippedCard);
-            })
-            flippedCards = [];
-            changeTurns();
-          }, 500);
-        }
+    
+    selected.flipped = true;
+
+    flippedCards.push(selected);
+    if (flippedCards.length >= 2) {
+      if (flippedCards[0].number === flippedCards[1].number) {
+        currentPlayer.hasControl = false;
+        setTimeout(function() {
+          currentPlayer.hasControl = true;
+          var $matchedCardPair = $('<div class="matched-card-pair"></div>');
+          currentPlayer.matchCount++;
+          flippedCards.forEach(function(flippedCard) {
+            flippedCard.matched = true;
+            flippedCard.getElement().replaceWith(Card.getBlankSpace());
+            $matchedCardPair.append(flippedCard.getHtml());
+          })
+          $('.' + currentPlayer.name + '-matched-cards').append($matchedCardPair);
+          if (currentPlayer === computer && !isGameOver()) {
+            computer.makeMoves();
+          }
+          flippedCards = [];
+          renderSidebar();
+        }, 50);
+      } else {
+        currentPlayer.hasControl = false;
+        setTimeout(function() {
+          currentPlayer.hasControl = true;
+          flippedCards.forEach(function(flippedCard) {
+            flippedCard.flipped = false;
+            flippedCard.getElement().replaceWith(flippedCard.getHtml())
+            computer.addKnownCard(flippedCard);
+          })
+          flippedCards = [];
+          changeTurns();
+        }, 500);
       }
-    } else {
-      flippedCards.splice(flippedCards.indexOf(selected), 1);
     }
+ 
     $(this).replaceWith(selected.getHtml())
-    renderSidebar();
   }
+  renderSidebar();
 });
